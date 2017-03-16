ca831dab7d5c9b6700c1114813e3f7c3cc162dde
João Vieira <2121089@my.ipleiria.pt>
2017-03-16 08:45:17 +0000

    hgjgh

--- ProjetoMedAc/AlertSystem/Form1.cs
+++ ProjetoMedAc/AlertSystem/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AlertSystem
 {
-    public partial class Form1 : Form
+    public partial class FormAlertSystem : Form
     {
-        public Form1()
+        public FormAlertSystem()
         {
             InitializeComponent();
         }
