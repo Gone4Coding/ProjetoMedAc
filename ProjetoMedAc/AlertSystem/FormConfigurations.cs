006ba57e0923af71a8bf0f8e16b1402167ac2dd9
gonga <gonga@DESKTOP-KISGEL3>
2017-04-06 22:13:24 +0100

    sd

--- ProjetoMedAc/AlertSystem/FormConfigurations.cs
+++ ProjetoMedAc/AlertSystem/FormConfigurations.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AlertSystem.ServiceReferenceHealth;
 
 namespace AlertSystem
 {
@@ -34,7 +35,34 @@ namespace AlertSystem
 
         private void FillValues()
         {
-            
+            ServiceHealthAlertClient client = FormAlertSystem.GetClient();
+            List<ConfigurationLimitType> configurationLimitList = client.GetConfigurationLimitList();
+            List<Event> eventsList = client.GetEventList();
+
+            foreach (ConfigurationLimitType limitType in configurationLimitList)
+            {
+                switch (limitType.ConfigurationType.ToString())
+                {
+                    case "HR":
+                        numbNormalRateMin.Value = limitType.MinimumValue;
+                        numbNormalRateMax.Value = limitType.MaximumValue;
+                        numbAnyTimeRateMin.Value = limitType.MinimumCriticalValue;
+                        numbAnyTimeRateMax.Value = limitType.MaximumCriticalValue;
+                        break;
+
+                    case "SPO2":
+                        numbNormalSaturation.Value = limitType.MinimumValue;
+                        numbAnyTimeSaturation.Value = limitType.MinimumCriticalValue;
+                        break;
+
+                    case "BP":
+                        numbNormalDiastolic.Value = limitType.MinimumValue;
+                        numbNormalSystolic.Value = limitType.MaximumValue;
+                        numbAnyTimeDiastolic.Value = limitType.MinimumCriticalValue;
+                        numbAnyTimeSystolic.Value = limitType.MaximumCriticalValue;
+                        break;
+                }
+            }
         }
 
         private bool ValdiadeParameters()
